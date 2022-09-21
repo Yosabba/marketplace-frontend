6ec3220044c350d9d22db271deac27f2c1b630d2
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-20 20:08:23 -0400

    created footer component to be used on all pages

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center bg-black h-12 mt-20  w-full">
+      <p className="text-white text-xs font-thin"> &copy; Copyright 2022</p>
+    </footer>
+  );
+};
+
+export default Footer;
