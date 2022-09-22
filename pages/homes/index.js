71c734a3df7d24fb7bac8280faf9986a8dc86494
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-22 14:37:09 -0400

    :hammer: refactored house card into component

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -35,20 +35,9 @@ export default function Home({ data }) {
 
       <section className="grid grid-cols-auto-fit gap-7 justify-items-center mx-3">
         {houses.map((house) => {
-          return (
-            <section
-              className="flex flex-col justify-center items-center border-2 border-gray-100 rounded-lg transition duration-500 ease-in-out hover:shadow-md cursor-pointer m-8"
-              key={house.listingid}
-            >
-              {/* <Image src={house.image_url} width={300} height={400} /> */}
-
-              
-            </section>
-          );
+          return <HouseCard house={house} /> ;
         })}
-
       </section>
-
     </main>
   );
 }
