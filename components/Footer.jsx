b993c9d595c5b946b410fcc3f78bdaf1872e99da
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-29 16:35:00 -0400

    gave the margin on the footer smaller value

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center bg-black h-12 mt-20 w-full">
+    <footer className="flex flex-col items-center justify-center bg-black h-12 mt-12 w-full">
       <p className="text-white text-xs font-thin"> &copy; Copyright 2022</p>
     </footer>
   );
