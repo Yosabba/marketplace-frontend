4e82ea5850f37baf6cf9b0626a56713b70f8ec53
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-01 16:14:38 -0400

    made footer fixed to the bottom of the page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center bg-black h-12 mt-12 w-full">
+    <footer className="flex flex-col items-center justify-center bg-black h-12 mt-12 w-full bottom-0 fixed">
       <p className="text-white text-xs font-thin"> &copy; Copyright 2022</p>
     </footer>
   );
