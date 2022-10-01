7ef351bddd5cbfe361b0a0bd28a3ac7a91473eb5
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-01 14:50:31 -0400

    formatted image tag

--- components/cards/HouseCard.jsx
+++ components/cards/HouseCard.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-
 const HouseCard = ({ house }) => {
   function housePriceToComma(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
@@ -8,14 +7,17 @@ const HouseCard = ({ house }) => {
   return (
     <section className="flex flex-col border-2 border-gray-100 rounded-lg transition duration-500 ease-in-out hover:shadow-md cursor-pointer w-96 min-h-[30vh] p-6">
       {/* <Image src={house.image_url} alt="image of the house property" width={300} height={300} className="pb-5" /> */}
-      <img src={house.image_url} alt="image of the house property" className="rounded-xl hover:scale-105 py-2 transition duration-500 ease-in-out w-full " />
+      <img
+        src={house.image_url}
+        alt="image of the house property"
+        className="rounded-xl hover:scale-105 py-2 transition duration-500 ease-in-out w-full "
+      />
       <h1 className=" font-bold text-lg text-left ">
         ${housePriceToComma(house.price)}
       </h1>
       {/* zoom on image when hovered */}
 
       <div className="flex flex-row">
-
         <div class="inline-flex flex-row items-start justify-center rounded-md">
           <button
             type="button"
