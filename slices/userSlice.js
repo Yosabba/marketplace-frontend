48178c62a9f63a69aec2139c350fee4e3cc7f8cb
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-03 23:26:38 -0400

    updated user slice for redux store

--- slices/userSlice.js
+++ slices/userSlice.js
@@ -1,22 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, isRejected } from "@reduxjs/toolkit";
+import userService from "./userService";
+import axios from "axios";
 
 const initialState = {
-  value: true,
+  user: "",
+  isLoggedIn: false,
 };
+
+export const signUserIn = createAsyncThunk(
+  "user/login",
+  async (user, thunkApi) => {
+    // try {
+    //   return await userService.signUserIn(user);
+    // } catch ({ message }) {
+    //   return thunkApi.rejectWithValue(message);
+    // }
+    try {
+      const { data } = await axios.post("http://localhost:5000/login", user, {
+        withCredentials: true,
+      });
+
+      if (data) {
+        localStorage.setItem("token", JSON.stringify(data.token));
+        localStorage.setItem("user", JSON.stringify(data.user));
+      }
+      console.log("etbbte");
+      return data;
+    } catch ({ message }) {
+      return message;
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: "userLoggedIn",
   initialState,
   reducers: {
     logUserOut: (state) => {
-      state.value = false;
+      state.user = false;
     },
     logUserIn: (state) => {
-      state.value = true;
+      state.isLoggedIn = true;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUserIn.rejected, (state) => {
+        state.isLoggedIn = false;
+      })
+      .addCase(signUserIn.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.user = action.payload;
+      })
+      .addCase(signUserIn.pending, (state) => {
+        state.isLoggedIn = false;
+      });
   },
 });
 
-export const { logUserOut, logUserIn } = userSlice.actions;
+export const { logUserOut, logUserIn, changeMessage } = userSlice.actions;
 
 export default userSlice.reducer;
