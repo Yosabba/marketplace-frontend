33c968e0be3d45e9819eef0fecea3865ead85f01
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-04 17:37:04 -0400

    added user effect to check and see if user is logged in or not. If not send them to login page

--- pages/for-sale/[id].js
+++ pages/for-sale/[id].js
@@ -1,9 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+
 const HouseDetails = () => {
-    return ( 
-        <div>
-            <h1>for sale house details</h1>
-        </div>
-     );
-}
- 
+  const router = useRouter();
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("token");
+    isLoggedIn ? localStorage.getItem("user") : router.push("/login");
+  }, [isLoggedIn]);
+
+  return (
+    <div>
+      <h1>for sale house details</h1>
+    </div>
+  );
+};
+
 export default HouseDetails;
--- pages/for-sale/index.js
+++ pages/for-sale/index.js
@@ -2,19 +2,30 @@ import Head from "next/head";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-// export const getStaticProps = async () => {
-//   const response = await axios.get("http://localhost:5000/houses");
-//   const data = response.data;
 
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// };
+export const getStaticProps = async () => {
+  const response = await axios.get("http://localhost:5000/houses");
+  const data = response.data;
 
-export default function ForSale() {
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function ForSale({ data = null }) {
+  const router = useRouter();
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    isLoggedIn ? localStorage.getItem("user") : router.push("/login");
+  }, [isLoggedIn]);
+
   return (
     <main className="pt-12 h-screen">
       <Head>
@@ -25,7 +36,6 @@ export default function ForSale() {
 
       <section className="flex mt-4 mb-4 flex-col items-center">
         <h1 className=" text-6xl font-semibold mb-20">For Sale Page</h1>
-
       </section>
     </main>
   );
