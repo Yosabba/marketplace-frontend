aca34c5ec80365e2a8d4ee31c6fc211c8f97a5b3
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-10 00:50:44 -0400

    added condition to check if user is logged in

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
 
 const Header = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+
   const headerWrapper = "flex justify-between items-center py-4 px-6 bg-white";
 
   const linkStyle =
-    "mx-2 capitalize transition duration-300 ease-in-out hover:text-gray-300 text-lg";
+    "mx-2 capitalize transition duration-300 ease-in-out hover:text-gray-600 text-lg";
 
   const btnStyle =
     "bg-blue-700 p-2 rounded-lg text-slate-200 ml-4 transition duration-500 ease-in-out hover:bg-blue-800 ";
@@ -30,9 +33,15 @@ const Header = () => {
         <Link href="/for-sale">
           <a className={linkStyle}>Sell</a>
         </Link>
-        <Link href="/login">
-          <button className={btnStyle}>Sign In</button>
-        </Link>
+        {isLoggedIn ? (
+          <Link href="/profile">
+            <a>My Profile</a>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <a className={btnStyle}>Sign In</a>
+          </Link>
+        )}
       </div>
     </header>
   );
