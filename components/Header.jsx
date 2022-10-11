4851b596a3d5b1c79d8b64f42ea32c9afecb19c5
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-11 13:58:26 -0400

    checks to see if user is logged in or not when it mounts

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { logUserIn, logUserOut } from "../slices/userSlice";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const { isLoggedIn } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const headerWrapper = "flex justify-between items-center py-4 px-6 bg-white";
 
@@ -12,6 +18,20 @@ const Header = () => {
 
   const btnStyle =
     "bg-blue-700 p-2 rounded-lg text-slate-200 ml-4 transition duration-500 ease-in-out hover:bg-blue-800 ";
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+
+    user !== null && dispatch(logUserIn());
+  }, [isLoggedIn]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    localStorage.clear();
+    router.push("/");
+    dispatch(logUserOut());
+  };
 
   return (
     <header className={headerWrapper}>
@@ -34,9 +54,17 @@ const Header = () => {
           <a className={linkStyle}>Sell</a>
         </Link>
         {isLoggedIn ? (
-          <Link href="/profile">
-            <a>My Profile</a>
-          </Link>
+          <>
+            <Link href="/profile">
+              <a className={linkStyle}>My Profile</a>
+            </Link>
+            <a
+              onClick={handleClick}
+              className="bg-blue-500 rounded-xl p-2 mx-3 hover:bg-blue-600 hover:cursor-pointer"
+            >
+              Log Out
+            </a>
+          </>
         ) : (
           <Link href="/login">
             <a className={btnStyle}>Sign In</a>
