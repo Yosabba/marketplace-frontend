3f3c4a270b94a6e359211d1284dbef56b81bc1fb
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-11 10:54:57 -0400

    removed shadow on hover and border width

--- components/cards/HouseCard.jsx
+++ components/cards/HouseCard.jsx
@@ -9,15 +9,15 @@ const HouseCard = ({ house }) => {
   }
 
   return (
-    <section className="flex flex-col border-2 border-gray-100 rounded-lg transition duration-500 ease-in-out hover:shadow-md cursor-pointer w-96 min-h-[30vh] p-6">
+    <section className="flex flex-col rounded-lg transition duration-500 ease-in-out cursor-pointer w-96 min-h-[30vh] p-6">
       <Image
         src={house.image_url}
         alt="image of the house property"
         width={300}
         height={300}
-        className="rounded-xl hover:scale-125 py-2 transition duration-500 ease-in-out w-full "
+        className="rounded-xl hover:scale-125 hover:rounded-xl py-2 transition duration-500 ease-in-out w-full mb-20"
       />
-      <h1 className=" font-bold text-lg text-left ">
+      <h1 className=" mt-5 font-bold text-lg text-left ">
         ${housePriceToComma(house.price)}
       </h1>
       {/* zoom on image when hovered */}
