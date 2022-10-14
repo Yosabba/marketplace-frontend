09be55070c4b038796db06791999c67da61d29a5
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-14 18:01:43 -0400

    :bug: causing application to crash because localstorage wasnt defined when page reloaded

--- pages/search-houses.js
+++ pages/search-houses.js
@@ -1,12 +1,20 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
-import HouseCard from "../components/cards/HouseCard"
-export default function Home() {
+import HouseCard from "../components/cards/HouseCard";
+import { useEffect, useState } from "react";
 
+export default function SearchedHouses() {
+  const [searchParams, setSearchParams] = useState("");
   const { searchHouses } = useSelector((state) => state.user);
-  const location = localStorage.getItem("cityState");
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    searchHouses.length === 0
+      ? router.push("/")
+      : setSearchParams(localStorage.getItem("cityState"));
+  }, [searchHouses]);
+
   return (
     <main>
       <Head>
@@ -15,7 +23,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-center text-3xl">{`Houses in ${location}`}</h1>
+      <h1 className="text-center text-3xl">{`Houses in ${searchParams}`}</h1>
       {searchHouses.map((house) => (
         <HouseCard key={house.id} house={house} />
       ))}
